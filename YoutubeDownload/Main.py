af5368621212f164f2ba0587d13ebd10a2108ee3
Igor Vidal Meneghini <101738878+IgorMeneghini@users.noreply.github.com>
2022-12-01 16:24:40 -0300

    Add files via upload

---
+++ YoutubeDownload/Main.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+import youtube_dl
+resp = 1
+while resp > 0:
+    print("Insert the link")
+    link = input("")
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+
